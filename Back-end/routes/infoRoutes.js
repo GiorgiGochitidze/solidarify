98b08d326edaf745608db273cd8b3a14a654662f
nika <nika3141535@gmail.com>
2024-12-05 19:11:28 +0400

    add:info routes

--- Back-end/routes/infoRoutes.js
+++ Back-end/routes/infoRoutes.js
@@ -1,8 +1,17 @@
-const { getInfos } = require('../controllers/infoController')
+const { getInfos,deleteInfo,createInfo } = require('../controllers/infoController')
 const express = require('express')
 
 const router = express.Router()
 
-router.get('/', getInfos)
+router.use(protect)
+
+router
+    .route('/')
+    .get(getInfos)
+    .post(createInfo)
+
+router
+    .route('/:id')
+    .delete(deleteInfo)
 
 module.exports = router
