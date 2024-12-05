035d5805e3f6d8c388fe7cdacb36a0a8afecae00
nika <nika3141535@gmail.com>
2024-12-05 18:50:30 +0400

    add:missing persons controllers

--- Back-end/controllers/MissingController.js
+++ Back-end/controllers/MissingController.js
@@ -1,11 +1,47 @@
 const Missing = require('../models/Missing')
 
-const getMissingPersons = function (req, res) {
-  Missing.find()
-    .then((persons) => res.status(200).json(persons))
-    .catch((error) => res.status(500).json({ message: 'Error fetching missing persons' }))
-}
+// const getMissingPersons = function (req, res) {
+//   Missing.find()
+//     .then((persons) => res.status(200).json(persons))
+//     .catch((error) => res.status(500).json({ message: 'Error fetching missing persons' }))
+// }
 
-module.exports = {
-  getMissingPersons
-}
+exports.getMissingPersons = catchAsync(async(req,res,next)=>{
+  const doc = await Missing.find()
+
+  res.status(200).json({
+    status:'success',
+    result:doc.length,
+    data:doc
+  })
+
+})
+
+exports.deleteMissing = catchAsync(async(req,res,next)=>{
+
+  const doc = await Missing.findByIdAndDelete(req.params.id)
+
+  if(!doc){
+    return next(new AppError('no document found with that id',404))
+  }
+
+  res.status(204).josn({
+    status:'success'
+  })
+
+})
+
+exports.createMissing = catchAsync(async(req,res,next)=>{
+  
+  const doc = await Missing.create(req.body)
+
+  res.status(201).json({
+    status:'succes',
+    data:doc
+  })
+
+})
+
+// module.exports = {
+//   getMissingPersons
+// }
