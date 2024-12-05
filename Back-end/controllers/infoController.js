73badd716a2e87d9693e47938349cb84fa395250
nika <nika3141535@gmail.com>
2024-12-05 18:51:42 +0400

    add:infos controllers

--- Back-end/controllers/infoController.js
+++ Back-end/controllers/infoController.js
@@ -1,11 +1,48 @@
 const Info = require('../models/Info')
 
-const getInfos = function (req, res) {
-  Info.find()
-    .then((infos) => res.status(200).json(infos))
-    .catch((error) => res.status(500).json({ message: 'Error fetching infos' }))
-}
+// const getInfos = function (req, res) {
+//   Info.find()
+//     .then((infos) => res.status(200).json(infos))
+//     .catch((error) => res.status(500).json({ message: 'Error fetching infos' }))
+// }
 
-module.exports = {
-  getInfos
-}
+exports.getInfos = catchAsync(async(req,res,next)=>{
+  const doc = await Info.find()
+
+  res.status(200).json({
+    status:'success',
+    result:doc.length,
+    data:doc
+  })
+
+})
+
+
+exports.deleteInfo = catchAsync(async(req,res,next)=>{
+
+  const doc = await Info.findByIdAndDelete(req.params.id)
+
+  if(!doc){
+    return next(new AppError('no document found with that id',404))
+  }
+
+  res.status(204).josn({
+    status:'success'
+  })
+
+})
+
+exports.createInfo = catchAsync(async(req,res,next)=>{
+  
+  const doc = await Info.create(req.body)
+
+  res.status(201).json({
+    status:'succes',
+    data:doc
+  })
+
+})
+
+// module.exports = {
+//   getInfos
+// }
