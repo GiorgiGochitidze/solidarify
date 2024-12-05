4763351468501ae47fc498610baa2029b3d6f150
nika <nika3141535@gmail.com>
2024-12-05 19:19:21 +0400

    add:locaion routes

--- Back-end/routes/LocationRoutes.js
+++ Back-end/routes/LocationRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express')
-const { getLocations } = require('../controllers/LocationController')
+const { getLocations,deleteLocation,createLocation } = require('../controllers/LocationController')
+const {protect} = require('../controllers/authController')
+
 
 const router = express.Router()
 
-router.get('/', getLocations)
+router.use(protect)
+
+router
+    .route('/')    
+    .get(getLocations)
+    .post(createLocation)
+
+router
+    .route('/:id')
+    .delete(deleteLocation)
 
 module.exports = router
