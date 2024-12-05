50b1189a179d96562ec3f588860db4a1f52a97de
nika <nika3141535@gmail.com>
2024-12-05 16:53:36 +0400

    add:user controller

--- Back-end/controllers/userController.js
+++ Back-end/controllers/userController.js
@@ -1,1 +1,22 @@
 
+require('dotenv').config();
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+  
+  
+    if (!email || !password) {
+      return next(new AppError('please provide email and password', 400));
+    }
+  
+    if(email != process.env.EMAIL && password != process.env.PASS ){
+        return next(new AppError('incorrect email or password', 401));
+    }
+  
+  
+    res.status(200).json({
+      status: 'success',
+      token,
+    });
+    
+  });
