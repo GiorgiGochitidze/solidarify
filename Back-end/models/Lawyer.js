113b765817a0be1bfb80fda0478ef7d0496acece
nika <nika3141535@gmail.com>
2024-12-05 17:47:57 +0400

    fix:lawyer model

--- Back-end/models/Lawyer.js
+++ Back-end/models/Lawyer.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 const LawyerSchema = new mongoose.Schema({
+  full_Name:String,
   contact: String,
   details: String,
+  image:String,
   date: {
     type: Date,
     default: Date.now
